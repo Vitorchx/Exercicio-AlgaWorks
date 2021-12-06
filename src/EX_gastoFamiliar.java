a5f9ab16080b1cb1a46948218f0a7a92ed12db28
Vitor Rosa Coelho <92186842+Vitorchx@users.noreply.github.com>
2021-12-06 17:48:34 -0300

    Update EX_gastoFamiliar.java
    Ajustes na formatação do texto apenas.

--- src/EX_gastoFamiliar.java
+++ src/EX_gastoFamiliar.java
@@ -13,12 +13,15 @@ public class EX_gastoFamiliar {
         System.out.println("Digite o valor da conta de água: ");
         Double contaAgua = scan.nextDouble();
         valorTotal += contaAgua;
+        
         System.out.println("Digite o valor da conta de telefone: ");
         Double contaTelefone = scan.nextDouble();
         valorTotal += contaTelefone;
+        
         System.out.println("Digite o valor da mensalidade da escola dos filhos: ");
         Double contaEscola = scan.nextDouble();
         valorTotal += contaEscola;
+        
         System.out.println("Total de gastos é de: " + valorTotal);
     }
 }
